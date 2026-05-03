1dade8af97dfc7a292a08f0c48c0dac7903dfd44
Eshanto Ghop Apurbo <eshantoghopapurbo@gmail.com>
2026-05-03 23:57:50 +0600

    page profile edit

--- src/proxy.js
+++ src/proxy.js
@@ -17,5 +17,5 @@ export async function proxy(request) {
 // export default function proxy(request) { ... }
  
 export const config = {
-  matcher: ["/profile","/tiles/:id"],
+  matcher: ["/profile","/tiles/:id*"],
 }
